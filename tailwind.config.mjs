d160de2d9a92ca7b200bd95b3e055da79d49bd46
nexxdevv <cliff.sanchez.999@gmail.com>
2024-12-17 14:32:15 -0800

    cleanup boilerplate

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,15 +3,10 @@ export default {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+    extend: {}
   },
-  plugins: [],
-};
+  plugins: []
+}
